da62c1b198feaf95ffb43211493c1bc21fed69da
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2023-07-08 16:22:21 +0000

    changes

--- src/models.py
+++ src/models.py
@@ -1,29 +1,47 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float,DateTime, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Product(Base): 
+    __tablename__ = "product"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250),nullable= False)
+    pricing = Column(Float, nullable = False)
+    weight = Column(Float)
+    color = Column(String(250), nullable= False)
+    shopping_cart = Column(Integer, ForeignKey("shopping_cart"))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Customer(Base):
+    __tablename__ = "customer"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    first_name = Column(String(250), nullable= False)
+    last_name = Column(String(250), nullable= False)
+    email = Column(String(250),nullable= False, unique=True)
+    address = Column(String(250), nullable=False)
+    shopping_cart = Column(Integer, ForeignKey("shopping_cart"))
+
+
+class Bill(Base):
+    __tablename__ = "bill"
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    total_price = Column(Float, nullable= False)
+    status = Column(String(30), Enum("paid","pending","refunded"))
+    shopping_cart = Column(Integer, ForeignKey("shopping_cart"))
+
+
+class shopping_cart(Base): 
+    __tablename__ = "shopping_cart" 
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer)
+    price = Column(Float)
+
 
     def to_dict(self):
         return {}
